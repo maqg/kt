4ad25afb67621feea7d296c9fa5bfc6ba5b7506e
maqg <maquangang@163.com>
2018-06-27 23:53:29 +0800

    add koa routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import * as Koa from 'koa';
+
+import * as Router from 'koa-router'
+
 const app = new Koa();
-app.use(context => {
-    context.body = "hello world";
-});
+
+/*
+
+ app.use(context => {
+ context.body = "hello world kt with api gateway";
+ });
+ */
+
+const router = new Router()
+
+router.get('/api/', async(ctx, next) => {
+ ctx.body = "Hello World, KT!"
+})
+
+app.use(router.routes())
+
 app.listen(8000);
+
 console.log("hello world");
