7a675f595336cd589d7f23e6f3a1f72f553962ad
maqg <maquangang@163.com>
2018-06-28 15:05:37 +0800

    maqg add api test program

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router'
+import * as KoaStatic from 'koa-static'
 import { Config } from './config/config'
 import {ApiDispatcher, InitApi} from "./api/api";
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/api/', (ctx, next) => {
-	return ApiDispatcher(ctx);
+router.get('/api/', async(ctx, next) => {
+	await ApiDispatcher(ctx);
 });
+
+app.use(KoaStatic("./static"));
 
 app.use(router.routes());
 app.use(router.allowedMethods());
