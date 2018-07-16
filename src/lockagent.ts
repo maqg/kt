d31c902db2c6eef62ab4f34673105d5ec5c9515f
maqg <maquangang@gmail.com>
2018-07-16 17:38:39 +0800

    for lock agent and lock monitor.

--- src/lockagent.ts
+++ src/lockagent.ts
@@ -9,22 +9,30 @@
 import * as Net from 'net';
 import {Config} from "./config/config";
 
-var quitting = false;
-var conn;
-var retryTimeout = 3000;	// 三秒，定义三秒后重新连接
-var retriedTimes = 0;	// 记录重新连接的次数
-var maxRetries = 10;	// 最多重新连接十次
+let quitting = false;
+let conn;
+let retryTimeout = 3000;	// 三秒，定义三秒后重新连接
+let retriedTimes = 0;	// 记录重新连接的次数
+let maxRetries = 10;	// 最多重新连接十次
+
+const IMEI = "imei123456789XX";
 
 process.stdin.resume();	//process.stdin流来接受用户的键盘输入，这个可读流初始化时处于暂停状态，调用流上的resume()方法来恢复流
 
-process.stdin.on('data', function(data){
-	if (data.toString().trim().toLowerCase() === 'quit'){
+process.stdin.on("data", function (data) {
+	if (data.toString().trim().toLowerCase() === "quit") {
 		quitting = true;
-		console.log('quitting');
+		console.log("quitting");
 		conn.end();
 		process.stdin.pause();
 	} else {
-		conn.write(data);
+		let obj = {
+			"imei": IMEI,
+			"voltage": 1.2,
+			"mac": "11:22:33:44:55:66",
+			"data": data,
+		};
+		conn.write(JSON.stringify(obj));
 	}
 });
 
@@ -32,26 +40,26 @@ process.stdin.on('data', function(data){
 (function connect() {
 	function reconnect() {
 		if (retriedTimes >= maxRetries) {
-			throw new Error('Max retries have been exceeded, I give up.');
+			throw new Error("Max retries have been exceeded, I give up.");
 		}
-		retriedTimes +=1;
+		retriedTimes += 1;
 		setTimeout(connect, retryTimeout);
 	}
 
 	conn = Net.createConnection(Config.LockMsgListenPort);
 
-	conn.on('connect', function() {
+	conn.on("connect", function () {
 		retriedTimes = 0;
-		console.log('connect to server');
+		console.log("connect to server");
 	});
 
-	conn.on('error', function(err) {
-		console.log('Error in connection:', err);
+	conn.on("error", function (err) {
+		console.log("Error in connection:", err.toString());
 	});
 
-	conn.on('close', function() {
-		if(! quitting) {
-			console.log('connection got closed, will try to reconnect');
+	conn.on("close", function () {
+		if (!quitting) {
+			console.log("connection got closed, will try to reconnect");
 			reconnect();
 		}
 	});
--- src/lockmonitor.ts
+++ src/lockmonitor.ts
@@ -8,19 +8,52 @@
 
 import * as Net from 'net';
 import {Config} from "./config/config";
+import {Bike} from "./models/Bike";
 
 let server = Net.createServer();
+
+class LockSocket {
+	socket: any;
+	imei: string;
+	bike: Bike;
+
+	constructor (socket) {
+		this.socket = socket;
+	}
+
+	public key () {
+		return this.socket.remoteAddress + "#" + this.socket.remotePort;
+	}
+}
 
 //聚合所有客户端
 let sockets = [];
 
+let socketMap = {};
+
 //接受新的客户端连接
-server.on('connection', function(socket){
-	console.log('got a new connection');
+server.on("connection", function(socket){
+
+	let lockSocket = new LockSocket(socket);
+
+	console.log("got a new connection, Address: "
+		+ socket.remoteAddress + "Port: "
+		+ socket.remotePort);
+
+	socketMap[lockSocket.key()] = lockSocket;
 	sockets.push(socket);
-	//从连接中读取数据
-	socket.on('data', function(data){
-		console.log('got data:', data);
+
+	// 从连接中读取数据
+	socket.on("data", function(data) {
+
+		let key = socket.remoteAddress + "#" + socket.remotePort;
+		if (socketMap.hasOwnProperty(key)) {
+			console.log("got data from socket " + key);
+		} else {
+			console.log("no socket found for " + key);
+		}
+
+		console.log("got data:", data.toString());
 
 		//广播数据
 		//每当一个已连接的用户输入数据，就将这些数据广播给其他所有已连接的用户
@@ -31,22 +64,24 @@ server.on('connection', function(socket){
 		});
 
 		//删除被关闭的连接
-		socket.on('close', function(){
-			console.log('connection closed');
+		socket.on("close", function() {
+			console.log("connection closed");
 			let index = sockets.indexOf(socket);
 			sockets.splice(index, 1);
 		});
 	});
 });
 
-server.on('error', function(err){
-	console.log('Server error:', err.message);
+server.on("error", function(err){
+	console.log("Server error:", err.message);
 });
 
-server.on('close', function(){
-	console.log('Server closed');
+server.on("close", function(){
+	console.log("Server closed");
 });
 
-console.log("Lock Monitor Start to run on " + Config.LockMsgListenPort);
+server.on("listening", function () {
+	console.log("Server Listening on port " + Config.LockMsgListenPort);
+});
 
 server.listen(Config.LockMsgListenPort);
